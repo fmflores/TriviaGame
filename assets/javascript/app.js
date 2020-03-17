d65d171a52528e4305281584a1b8275ed8914615
Frank Flores <frankthetank67@gmail.com>
2020-03-16 21:11:03 -0700

    added comments to my js code to explain what certain parts of my code does

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 //global variables
+// array of questions with objects containing each question, the choices for the questions answers, and the index of the correct answer 
 var questionsArr = [
   {
     q: "What is Michael Scott's convict alterego/character?",
@@ -42,35 +43,47 @@ let right = 0;
 let wrong = 0;
 let timer;
 
+// 
 $("#lose-image").hide();
 $("#win-image").hide();
 
 //init game function
 function init() {
+  // if the counter is equal to the number of questions/length of the questionsArr end the game
   if (counter === questionsArr.length) {
     endGame();
   }
+// selecting the questions div, write to the html 
   $(".questions").html(
+    // create a div with the id of qTitle and insert the current index of the questionsArr (the question) and create a div with id of "answers" 
     `<div id='qTitle'>${questionsArr[counter].q}</div><div id='answers'></div>`
   );
+  // for each choice (c) of possible answers of the current index 
   questionsArr[counter].c.forEach(function(answer, i) {
+    // append to the answers div, a button with the id of the current index, and write the text of its javascript code to the button
     $("#answers").append(
       `<button id=${i} class='answerBtn'>${answer}</button>`
     );
   });
 }
 
+// when the start button is clicked
 $("#start-button").on("click", function() {
+  // hide the start-game div which holds the start button
   $(".start-game").css("display", "none");
+  // and run the setInterval function with the arguments of count function and run every second
   timer = setInterval(count, 1000);
   init();
 });
 
+// apply to the whole document. onclick of the answerBtn, 
 $(document).on("click", ".answerBtn", function() {
-  console.log($(this).attr("id"));
+  // when the button is clicked if id of the button being clicked is equal to the answer (a) key of the current question (equal in value) 
   if ($(this).attr("id") == questionsArr[counter].a) {
+    // run the correct function
     correct();
   } else {
+    // else run the incorrect function
     incorrect();
   }
 });
@@ -97,8 +110,11 @@ function count() {
 
 function endGame() {
   clearInterval(timer);
+  // write to the questions div 
   $(".questions").html(
+    // create an h1 element with the text Right and Wrong and insert the javascript code of number of right and num of wrong
     `<h1>Right: ${right} <br/> Wrong: ${wrong} <br/>  YOU ${
+      // ternary operator: if right > wrong display WIN else display LOSE!
       right > wrong ? "WIN!" : "LOSE!"
     }</h1>`
   );
